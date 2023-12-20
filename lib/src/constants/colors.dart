56c0060b65f962546c27efc8d19fc79374519c4c
Asem <asemqoraishi.b@gmail.com>
2023-12-20 07:28:16 +0430

    created app colors for the dark and the light themes

---
+++ lib/src/constants/colors.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class AppLightColors {
+  AppLightColors._();
+
+  static const primaryColor = Color.fromRGBO(255, 255, 255, 1);
+  static const scondryColor = Color.fromRGBO(204, 204, 204, 1);
+  static const primaryText = Color.fromRGBO(30, 30, 30, 1);
+  static const secondryText = Color.fromRGBO(102, 102, 102, 1);
+  static const accentColor1 = Color.fromRGBO(52, 152, 219, 1);
+  static const errorColor = Colors.deepOrangeAccent;
+  static const warningColor = Colors.orangeAccent;
+  static const buttonColor = Color.fromRGBO(39, 174, 96, 1);
+  static const borderColor = Color.fromRGBO(30, 30, 30, 1);
+}
+
+class AppDarkColors {
+  AppDarkColors._();
+
+  static const primaryColor = Color.fromRGBO(20, 20, 20, 1);
+  static const scondryColor = Color.fromRGBO(46, 46, 46, 1);
+  static const primaryText = Colors.white;
+  static const secondryText = Color.fromRGBO(204, 204, 204, 1);
+  static const accentColor1 = Color.fromRGBO(52, 152, 219, 1);
+  static const errorColor = Colors.deepOrangeAccent;
+  static const warningColor = Colors.orangeAccent;
+  static const buttonColor = Color.fromRGBO(39, 174, 96, 1);
+  static const borderColor = Color.fromRGBO(204, 204, 204, 1);
+}
