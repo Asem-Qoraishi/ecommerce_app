a93700824751ea414d8d3e53add0baac8b5730f0
Asem <asemqoraishi.b@gmail.com>
2023-12-25 16:55:45 +0430

    a widget to show product purchase date along with button to leave a review.

---
+++ lib/src/features/products/presentation/product_screen/leave_review_action.dart
@@ -0,0 +1,48 @@
+import 'package:ecommerce_app/src/constants/app_sizes.dart';
+import 'package:ecommerce_app/src/constants/colors.dart';
+import 'package:ecommerce_app/src/core/utils/date_fromatter.dart';
+import 'package:ecommerce_app/src/core/widgets/custom_text_button.dart';
+import 'package:ecommerce_app/src/core/widgets/responsive_two_column_layout.dart';
+import 'package:ecommerce_app/src/features/orders/domain/purchase.dart';
+import 'package:flutter/material.dart';
+
+class LeaveReviewAction extends StatelessWidget {
+  const LeaveReviewAction({Key? key, required this.productId}) : super(key: key);
+  final String productId;
+  @override
+  Widget build(BuildContext context) {
+    //TODO: should read from datasource
+    final purchase = Purchase(orderId: 'Abc', orderDate: DateTime.now());
+    if (purchase != null) {
+      final formattedDate = HDateFromater.format(purchase.orderDate);
+      return Column(
+        children: [
+          const Divider(),
+          gapH8,
+          ResponsiveTwoColumnLayout(
+            startFlex: 3,
+            endFlex: 2,
+            breakpoint: 300,
+            rowMainAxisAlignment: MainAxisAlignment.spaceBetween,
+            rowCrossAxisAlignment: CrossAxisAlignment.center,
+            columnCrossAxisAlignment: CrossAxisAlignment.center,
+            spacing: Sizes.p16,
+            startWidget: Text(
+              'Purchased on $formattedDate',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            endWidget: CustomTextButton(
+              text: 'Leave a review',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(color: AppDarkColors.buttonColor),
+              onPressed: () {
+                ///TODO : GO to the leave review screen
+              },
+            ),
+          )
+        ],
+      );
+    } else {
+      return const SizedBox();
+    }
+  }
+}
