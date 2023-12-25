31354fe5b54d41d85d736df5094771aaa9c8ea8d
Asem <asemqoraishi.b@gmail.com>
2023-12-25 16:44:15 +0430

    Designed responsive reusable widgits

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,7 @@
-import 'package:ecommerce_app/src/config/theme/theme.dart';
-import 'package:ecommerce_app/src/features/products/presentation/products_list/products_list_screen.dart';
+import 'package:ecommerce_app/src/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      themeMode: ThemeMode.system,
-      theme: AppTheme.lightTheme,
-      darkTheme: AppTheme.darkTheme,
-      home: const ProductsListScreen(),
-    );
-  }
 }
