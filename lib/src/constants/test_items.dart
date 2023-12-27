630d3938445ee00199be62acbd3af23c001187eb
Asem <asemqoraishi.b@gmail.com>
2023-12-27 12:16:05 +0430

    Global constant items for testing until implementing data source

---
+++ lib/src/constants/test_items.dart
@@ -0,0 +1,17 @@
+import 'package:ecommerce_app/src/features/cart/domain/item.dart';
+
+//Constants Itmes to be used in screens until we implement datasource
+const kTestItems = [
+  Item(
+    productId: '1',
+    quantity: 3,
+  ),
+  Item(
+    productId: '2',
+    quantity: 4,
+  ),
+  Item(
+    productId: '3',
+    quantity: 2,
+  ),
+];
